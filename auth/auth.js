5492f0aba7da062b607d17f8cf7a940e96ffb60d
Marquis Pierre <marquispierre27@gmail.com>
2019-09-16 20:04:56 -0400

    grid assignment

---
+++ auth/auth.js
@@ -0,0 +1,14 @@
+const loginButton = document.getElementById("login-button");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const message = document.getElementById("login-message");
+
+
+
+
+loginButton.onclick = function(event){
+ message.textContent = emailInput.value + " has logged in and ready to rumble:) ";
+};
+
+
+
