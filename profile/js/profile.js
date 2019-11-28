f88dc222c5807911af14a6e06de8748cdb327cac
MarquisPierre <43345914+MarquisPierre@users.noreply.github.com>
2019-11-27 22:03:13 -0500

    Add files via upload

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -2,22 +2,35 @@ const uid = location.search.split('=')[1];
 const userRef = firebase.database().ref('users').child(uid);
 
 const profileName = document.getElementById('profile-name');
+const profileEmail = document.getElementById('profile-email');
+const profilePassword = document.getElementById('profile-password');
 const bioInput = document.getElementById('bio');
 const updateButton = document.getElementById('update-profile');
 
 userRef.on('value', function(snapshot) {
+	console.log(snapshot);
 	const userInfo = snapshot.val();
 	profileName.value = userInfo.displayName;
 	
+	
+	console.log(userInfo);
 	if (userInfo.bio) {
 		bioInput.value = userInfo.bio;
+	}
+	if (userInfo.displayPassword){
+		profilePassword.value = userInfo.displayPassword;
+	}
+	if (userInfo.displayEmail){
+		profileEmail.value = userInfo.displayEmail;
 	}
 });
 
 updateButton.onclick = function() {
 	userRef.update({
 		displayName: profileName.value,
-		bio: bioInput.value
+		bio: bioInput.value,
+		displayEmail: profileEmail.value,
+		displayPassword: profilePassword.value
 	});
 };
 
