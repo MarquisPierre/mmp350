34ceaa7546ccfd86817362f5b950857aaf1f2021
MarquisPierre <43345914+MarquisPierre@users.noreply.github.com>
2019-12-18 22:31:18 -0500

    Add files via upload

---
+++ typography /js/create.js
@@ -0,0 +1,29 @@
+const userInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const createButton = document.getElementById("create-button");
+const errorMessage = document.getElementById("error-message");
+
+createButton.onclick = function() {
+	const promise = firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value);
+	promise.catch(function(error) {
+		errorMessage.textContent = error.message;
+	});
+	promise.then(function(credential) {
+        console.log(credential);
+        creatUser(credential.user.uid);
+		
+	});
+};
+
+function creatUser(uid){
+    const db = firebase.database();
+    const ref = db.ref('users').child(uid);
+    const promise = ref.update({
+        displayName: userInput.value
+    })
+    
+    promise.then(function(){
+        location.href ="index.html";
+    })
+}
